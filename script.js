34a2187eaeb548201308d32e82f85751fcd12ee1
Daniel Østensen <98233180+DanielSan-01@users.noreply.github.com>
2024-02-07 16:13:50 +0100

    chrismas edition

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("may 4, 2024 16:00:00").getTime();
+var countDownDate = new Date("dec 24, 2024 16:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
