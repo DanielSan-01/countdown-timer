ea43ef75041ffaf81b5c9246e70dc08ff477f055
Daniel Østensen <98233180+DanielSan-01@users.noreply.github.com>
2024-02-07 15:22:21 +0100

    functional but no styling

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const countDownDate = new Date("2024-05-04").getTime();
 
-var intervall = setInterval(function() {
-  var now = new Date().getTime(); //todays time
+let intervall = setInterval(function() {
+  let now = new Date().getTime(); //todays time
   //console.log(intervall);
   let distance = countDownDate - now; //distance between now and count date
 
-  const days = Math.floor(distance / (24 * 60 * 60 * 1000));
-  const hours = Math.floor(distance % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000);
-  const minutes = Math.floor(distance % (60 * 60 * 1000)) / (60 * 1000);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
   document.getElementById("countDownTimer").innerHTML = days + "d" + hours + "h" + minutes + "m";
 
   if (distance < 0) {
@@ -17,6 +21,10 @@ var intervall = setInterval(function() {
 
 }
 }, 500);
+console.log(distance);
 
 
-
+  /* const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+  const hours = Math.floor(distance % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000);
+  const minutes = Math.floor(distance % (60 * 60 * 1000)) / (60 * 1000);
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000); */
