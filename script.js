06a1ce646c12655b0accce9f03ea8bcce34c6fb8
Daniel Østensen <98233180+DanielSan-01@users.noreply.github.com>
2024-02-07 15:45:08 +0100

    fixed w3example

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const countDownDate = new Date("2024-05-04").getTime();
+// Set the date we're counting down to
+var countDownDate = new Date("may 4, 2024 16:00:00").getTime();
 
-let intervall = setInterval(function() {
-  let now = new Date().getTime(); //todays time
-  //console.log(intervall);
-  let distance = countDownDate - now; //distance between now and count date
+// Update the count down every 1 second
+var countdownfunction = setInterval(function() {
 
+  // Get todays date and time
+  var now = new Date().getTime();
+  
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+  
+  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-  document.getElementById("countDownTimer").innerHTML = days + "d" + hours + "h" + minutes + "m";
-
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  
+  // If the count down is over, write some text 
   if (distance < 0) {
-    clearInterval(intervall);
-    document.getElementById("countDownTimer").innerHTML = "Event is over";
-
-}
-}, 500);
-console.log(distance);
-
-
-  /* const days = Math.floor(distance / (24 * 60 * 60 * 1000));
-  const hours = Math.floor(distance % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000);
-  const minutes = Math.floor(distance % (60 * 60 * 1000)) / (60 * 1000);
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000); */
+    clearInterval(countdownfunction);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
